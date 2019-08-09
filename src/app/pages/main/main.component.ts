b7df67ce11234e0e15932521ceda0b0b100c88bb
Iñigo Gorosabel Fernandez <inigo.gorosabel@gmail.com>
2019-08-09 23:27:57 +0200

    Funcionalidad inicial en main

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./css/main.component.css']
 })
 export class MainComponent implements OnInit {
+  selectedOption: string = 'sales';
   constructor() {}
 
   ngOnInit() {}
+  
+  selectOptionSales(){
+    this.selectOption('sales');
+  }
+  
+  selectOptionItems(){
+    this.selectOption('items');
+  }
+  
+  selectOptionOrders(){
+    this.selectOption('orders');
+  }
+  
+  selectOption(option: string){
+    this.selectedOption = option;
+  }
 }
