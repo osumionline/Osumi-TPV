0bcefbdab75ee0853dd81eae5701aba59760c2e7
Iñigo Gorosabel Fernandez <inigo.gorosabel@gmail.com>
2019-08-11 02:08:26 +0200

    Casi termino página de instalación

--- src/app/pages/installation/installation.component.ts
+++ src/app/pages/installation/installation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogService } from '../../services/dialog.service';
 
 @Component({
   selector: 'app-installation',
@@ -6,6 +7,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./css/installation.component.css']
 })
 export class InstallationComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+  ivareOptions = [{id: 'iva', name: 'IVA'}, {id: 're', name: 'Recargo de equivalencia'}];
+  ivaOptionsList: number[] = [4, 10, 21];
+  reOptionsList: number[] = [4.5, 11.4, 26.2];
+
+  optionsList: number[] = [];
+  selectedOptionsList: number[] = [];
+
+  selectedOption: string = 'iva';
+  selectedOptionInList: number = null;
+  
+  marginList = [
+    {value: 20,  checked: false},
+    {value: 25,  checked: false},
+    {value: 30,  checked: false},
+    {value: 35,  checked: false},
+    {value: 40,  checked: false},
+    {value: 45,  checked: false},
+    {value: 50,  checked: false},
+    {value: 55,  checked: false},
+    {value: 60,  checked: false},
+    {value: 65,  checked: false},
+    {value: 70,  checked: false},
+    {value: 75,  checked: false},
+    {value: 80,  checked: false},
+    {value: 85,  checked: false},
+    {value: 90,  checked: false},
+    {value: 95,  checked: false},
+    {value: 100, checked: false}
+  ];
+  
+  hasOnline: string = '0';
+  hasExpiryDate: string = '0';
+
+  constructor(private dialog: DialogService) {}
+  ngOnInit() {
+    this.optionsList = [...this.ivaOptionsList];
+  }
+  
+  changeOptionList(id){
+    this.selectedOption = id;
+    this.selectedOptionsList = [];
+    this.selectedOptionInList = null;
+    
+    if (id==='iva'){
+      this.optionsList = [...this.ivaOptionsList];
+    }
+    if (id==='re'){
+      this.optionsList = [...this.reOptionsList];
+    }
+  }
+  
+  selectOption(item){
+    if (this.selectedOptionInList!=item){
+      this.selectedOptionInList = item;
+    }
+    else{
+      this.selectedOptionInList = null;
+    }
+  }
+  
+  addToList(){
+    let ind = this.optionsList.findIndex(x => x==this.selectedOptionInList);
+    if (ind==-1){
+      return;
+    }
+    this.selectedOptionsList.push(this.selectedOptionInList);
+    this.optionsList.splice(ind, 1);
+    this.selectedOptionInList = null;
+  }
+  
+  removeFromList(){
+    let ind = this.selectedOptionsList.findIndex(x => x==this.selectedOptionInList);
+    if (ind==-1){
+      return;
+    }
+    this.optionsList.push(this.selectedOptionInList);
+    this.selectedOptionsList.splice(ind, 1);
+    this.selectedOptionInList = null;
+  }
+  
+  saveConfiguration(){
+    if (this.selectedOptionsList.length==0){
+      this.dialog.alert({title: 'Error', content: '¡No has elegido ningún valor en la lista de IVA/Recargo de equivalencias!', ok: 'Continuar'}).subscribe(result => {});
+      return false;
+    }
+    
+    const selectedMargins = this.marginList.filter(x => x.checked);
+    console.log(selectedMargins);
+  }
 }
