5e4bafd00c41c716ee074a444bcec7dc2943ae17
Iñigo Gorosabel Fernandez <inigo.gorosabel@gmail.com>
2019-08-25 17:57:19 +0200

    Correcciones para movil y en códigos de barras

--- src/app/components/articulos/articulos.component.ts
+++ src/app/components/articulos/articulos.component.ts
@@ -346,7 +346,7 @@ export class ArticulosComponent implements OnInit {
   }
   
   deleteCodBarras(codBarras: CodigoBarras) {
-    let ind = this.articulo.codigosBarras.findIndex(x => x.codigoBarras = codBarras.codigoBarras);
+    let ind = this.articulo.codigosBarras.findIndex(x => x.codigoBarras == codBarras.codigoBarras);
     let newCodBarras = {
       id: null,
       codigoBarras: null,
