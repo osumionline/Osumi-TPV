da552b0d2b8fe3a3eb29d1eb07479dd5b01d53a8
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-03-02 12:57:01 +0100

    El nombre de la aplicación se toma de los datos de la instalación

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  name: 'Osumi TPV',
   apiUrl: 'https://apitpv.osumi.es/api/',
   fotosUrl: 'https://apitpv.osumi.es/fotos/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  name: 'Osumi TPV',
   apiUrl: 'https://apitpv.osumi.es/api/',
   fotosUrl: 'https://apitpv.osumi.es/fotos/'
 };
