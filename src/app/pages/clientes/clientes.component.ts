e9f4a4c797af60d3c4cb11a61b3dbeca5fa93009
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-10-27 16:33:25 +0200

    Empiezo apartado clientes

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Cliente } from "src/app/model/cliente.model";
+import { ClientesService } from "src/app/services/clientes.service";
 
 @Component({
-	selector: 'otpv-clientes',
-	templateUrl: './clientes.component.html',
-	styleUrls: ['./clientes.component.scss']
+  selector: "otpv-clientes",
+  templateUrl: "./clientes.component.html",
+  styleUrls: ["./clientes.component.scss"],
 })
 export class ClientesComponent implements OnInit {
-	constructor() {}
-	ngOnInit(): void {}
+  selectedClient: Cliente = null;
+
+  constructor(public cs: ClientesService) {}
+
+  ngOnInit(): void {
+    this.cs.load();
+  }
+
+  selectCliente(cliente: Cliente): void {
+    this.selectedClient = cliente;
+  }
 }
