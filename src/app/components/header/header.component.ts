ffc64f19553b8402cd88858dfe14aebe20779901
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-02 13:44:02 +0100

    Navegación entre apartados usando las teclas F5-F10

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ConfigService }            from 'src/app/services/config.service';
+import { Component, HostListener, Input, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ConfigService } from "src/app/services/config.service";
 
 @Component({
-	selector: 'otpv-header',
-	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.scss']
+  selector: "otpv-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
-	@Input() selectedOption: string = '';
+  @Input() selectedOption: string = "";
+  title: string;
 
-	constructor(public cs: ConfigService) {}
+  constructor(private cs: ConfigService, private router: Router) {}
 
-	ngOnInit(): void {}
+  ngOnInit(): void {
+    this.title = this.cs.nombre;
+  }
+
+  @HostListener("window:keydown", ["$event"])
+  onKeyDown(ev: KeyboardEvent): void {
+    const options: string[] = ["F5", "F6", "F7", "F8", "F9", "F10"];
+    if (options.includes(ev.key)) {
+      ev.preventDefault();
+      this.goTo(ev.key);
+    }
+  }
+
+  goTo(where: string): void {
+    const whereTo: {
+      [key: string]: string;
+    } = {
+      F5: "/ventas",
+      F6: "/articulos",
+      F7: "/pedidos",
+      F8: "/clientes",
+      F9: "/almacen",
+      F10: "/gestion",
+    };
+    this.router.navigate([whereTo[where]]);
+  }
 }
