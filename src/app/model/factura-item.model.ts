ce2a376a254c444bf6b0592a33aaef8da3f4b654
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-01-22 17:36:31 +0100

    Facturas: termino factura

---
+++ src/app/model/factura-item.model.ts
@@ -0,0 +1,15 @@
+export class FacturaItem {
+  deployed: boolean = false;
+
+  constructor(
+    public concepto: string = null,
+    public precioIVA: number = null,
+    public precioSinIVA: number = null,
+    public unidades: number = null,
+    public subtotal: number = null,
+    public iva: number = null,
+    public ivaImporte: number = null,
+    public total: number = null,
+    public lineas: FacturaItem[] = []
+  ) {}
+}
--- src/app/pages/factura/factura.component.ts
+++ src/app/pages/factura/factura.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Data, Params, Router } from "@angular/router";
+import { ActivatedRoute, Data, Params } from "@angular/router";
 import { IdSaveResult } from "src/app/interfaces/interfaces";
+import { FacturaItem } from "src/app/model/factura-item.model";
 import { Factura } from "src/app/model/factura.model";
 import { ClassMapperService } from "src/app/services/class-mapper.service";
 import { ClientesService } from "src/app/services/clientes.service";
@@ -17,10 +18,13 @@ export class FacturaComponent implements OnInit {
   preview: boolean = false;
   logoUrl: string = environment.baseUrl + "logo.jpg";
   factura: Factura = new Factura();
+  list: FacturaItem[] = [];
+  subtotal: number = 0;
+  ivas: { iva: number; importe: number }[] = [];
+  total: number = 0;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private router: Router,
     public config: ConfigService,
     private cs: ClientesService,
     private cms: ClassMapperService,
@@ -45,12 +49,70 @@ export class FacturaComponent implements OnInit {
   loadFactura(id: number): void {
     this.cs.getFactura(id).subscribe((result) => {
       this.factura = this.cms.getFactura(result.factura);
+      this.loadData();
       if (this.factura.impresa) {
         setTimeout(() => {
           window.print();
         });
       }
     });
+  }
+
+  loadData(): void {
+    for (let venta of this.factura.ventas) {
+      let ventaTemp = {};
+      for (let linea of venta.lineas) {
+        if (!ventaTemp.hasOwnProperty("iva_" + linea.iva)) {
+          ventaTemp["iva_" + linea.iva] = new FacturaItem();
+          ventaTemp["iva_" + linea.iva].concepto = "Ticket Nº " + venta.id;
+          ventaTemp["iva_" + linea.iva].iva = linea.iva;
+          ventaTemp["iva_" + linea.iva].unidades = 0;
+        }
+
+        let ventaLinea: FacturaItem = new FacturaItem();
+        ventaLinea.concepto = linea.articulo;
+        ventaLinea.precioIVA = linea.pvp;
+        ventaLinea.precioSinIVA = linea.pvp / ((100 + linea.iva) / 100);
+        ventaLinea.unidades = linea.unidades;
+        ventaLinea.subtotal = linea.unidades * ventaLinea.precioSinIVA;
+        ventaLinea.iva = linea.iva;
+        ventaLinea.ivaImporte =
+          linea.pvp * linea.unidades - ventaLinea.subtotal;
+        ventaLinea.total = linea.importe;
+
+        ventaTemp["iva_" + linea.iva].lineas.push(ventaLinea);
+      }
+      for (let iva in ventaTemp) {
+        for (let linea of ventaTemp[iva].lineas) {
+          ventaTemp[iva].precioIVA += linea.precioIVA;
+          ventaTemp[iva].precioSinIVA += linea.precioSinIVA;
+          ventaTemp[iva].unidades += linea.unidades;
+          ventaTemp[iva].subtotal += linea.subtotal;
+          ventaTemp[iva].ivaImporte += linea.ivaImporte;
+          ventaTemp[iva].total += linea.total;
+
+          this.subtotal += linea.subtotal;
+          this.addIva(linea.iva, linea.ivaImporte);
+          this.total += linea.total;
+        }
+        this.list.push(ventaTemp[iva]);
+      }
+    }
+  }
+
+  addIva(iva: number, importe: number): void {
+    let ind: number = this.ivas.findIndex((x): boolean => {
+      return x.iva === iva;
+    });
+    if (ind === -1) {
+      this.ivas.push({ iva, importe });
+    } else {
+      this.ivas[ind].importe += importe;
+    }
+  }
+
+  deploy(item: FacturaItem): void {
+    item.deployed = !item.deployed;
   }
 
   imprimir(): void {
