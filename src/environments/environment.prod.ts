006754dcdb5f9725794a5aa130672958ec6ca118
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-01-25 17:33:20 +0100

    Cambio configuración de prod a la real y creo configuración de test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: "https://apitpv.osumi.dev/",
-  apiUrl: "https://apitpv.osumi.dev/api",
-  fotosUrl: "https://apitpv.osumi.dev/fotos/",
-  pdfsUrl: "https://apitpv.osumi.dev/pdf/",
+  baseUrl: "http://apitpv/",
+  apiUrl: "http://apitpv/api",
+  fotosUrl: "http://apitpv/fotos/",
+  pdfsUrl: "http://apitpv/pdf/",
 };
