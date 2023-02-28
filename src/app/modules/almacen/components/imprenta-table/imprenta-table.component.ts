bbd4b5a331b47a39514ddf020f397d6c31354110
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-02-28 17:10:55 +0100

    Imprenta: empiezo a maquetar etiquetas

--- src/app/modules/almacen/components/imprenta-table/imprenta-table.component.ts
+++ src/app/modules/almacen/components/imprenta-table/imprenta-table.component.ts
@@ -1,15 +1,63 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
+import { Component } from "@angular/core";
+import { QRCodeModule } from "angularx-qrcode";
+import { ArticuloBuscador } from "src/app/model/articulos/articulo-buscador.model";
+import { FixedNumberPipe } from "src/app/modules/shared/pipes/fixed-number.pipe";
 
 @Component({
   standalone: true,
   selector: "otpv-imprenta-table",
   templateUrl: "./imprenta-table.component.html",
   styleUrls: ["./imprenta-table.component.scss"],
-  imports: [CommonModule],
+  imports: [CommonModule, QRCodeModule, FixedNumberPipe],
 })
 export class ImprentaTableComponent {
-  @Input() filas: number = 4;
-  @Input() columnas: number = 5;
-  @Input() mostrarPVP: boolean = true;
+  filas: number = 4;
+  columnas: number = 5;
+  list: ArticuloBuscador[][] = [];
+  mostrarPVP: boolean = true;
+
+  calcularLista(
+    filas: number,
+    columnas: number,
+    seleccionados: ArticuloBuscador[],
+    mostrarPVP: boolean
+  ): void {
+    this.filas = filas;
+    this.columnas = columnas;
+    this.mostrarPVP = mostrarPVP;
+
+    let f_ind: number = 0;
+    let c_ind: number = 0;
+    this.list = [];
+
+    for (let s of seleccionados) {
+      for (let ind: number = 0; ind < s.num; ind++) {
+        if (this.list[f_ind] === undefined) {
+          this.list[f_ind] = [];
+        }
+        this.list[f_ind][c_ind] = new ArticuloBuscador().fromInterface(
+          s.toInterface()
+        );
+        c_ind++;
+        if (c_ind === columnas) {
+          f_ind++;
+          c_ind = 0;
+        }
+      }
+    }
+
+    for (let f: number = 0; f < filas; f++) {
+      if (this.list[f] === undefined) {
+        this.list[f] = [];
+      }
+      for (let c: number = 0; c < columnas; c++) {
+        if (this.list[f][c] === undefined) {
+          this.list[f][c] = null;
+        }
+      }
+    }
+
+    console.log(this.list);
+  }
 }
--- src/app/modules/almacen/components/imprenta/imprenta.component.ts
+++ src/app/modules/almacen/components/imprenta/imprenta.component.ts
@@ -2,11 +2,13 @@ import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
 import { CommonModule } from "@angular/common";
 import { Component, ElementRef, ViewChild } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { ArticuloBuscadorResult } from "src/app/interfaces/articulo.interface";
 import { ArticuloBuscador } from "src/app/model/articulos/articulo-buscador.model";
+import { ImprentaTableComponent } from "src/app/modules/almacen/components/imprenta-table/imprenta-table.component";
 import { MaterialModule } from "src/app/modules/material/material.module";
 import { ClassMapperService } from "src/app/services/class-mapper.service";
+import { DialogService } from "src/app/services/dialog.service";
 import { VentasService } from "src/app/services/ventas.service";
-import { ImprentaTableComponent } from "../imprenta-table/imprenta-table.component";
 
 @Component({
   standalone: true,
@@ -21,22 +23,27 @@ export class ImprentaComponent {
   searchTimer: number = null;
   searching: boolean = false;
   articulos: ArticuloBuscador[] = [];
+  @ViewChild("tabla", { static: true }) tabla: ImprentaTableComponent;
   seleccionados: ArticuloBuscador[] = [];
   filas: number = 4;
   columnas: number = 5;
   mostrarPVP: boolean = true;
 
-  constructor(private vs: VentasService, private cms: ClassMapperService) {}
+  constructor(
+    private vs: VentasService,
+    private cms: ClassMapperService,
+    private dialog: DialogService
+  ) {}
 
   searchFocus(): void {
-    setTimeout(() => {
+    setTimeout((): void => {
       this.searchBox.nativeElement.focus();
     }, 100);
   }
 
   buscadorStart(): void {
     this.buscadorStop();
-    this.searchTimer = window.setTimeout(() => {
+    this.searchTimer = window.setTimeout((): void => {
       this.buscar();
     }, 300);
   }
@@ -58,10 +65,12 @@ export class ImprentaComponent {
   buscar(): void {
     this.buscadorStop();
     this.searching = true;
-    this.vs.search(this.search).subscribe((result) => {
-      this.searching = false;
-      this.articulos = this.cms.getArticulosBuscador(result.list);
-    });
+    this.vs
+      .search(this.search)
+      .subscribe((result: ArticuloBuscadorResult): void => {
+        this.searching = false;
+        this.articulos = this.cms.getArticulosBuscador(result.list);
+      });
   }
 
   selectArticulo(articulo: ArticuloBuscador): void {
@@ -76,11 +85,15 @@ export class ImprentaComponent {
     } else {
       this.seleccionados[ind].num++;
     }
+    this.updateTable();
   }
 
   addHueco(): void {
     const maxInd: number = this.seleccionados.length - 1;
-    if (this.seleccionados[maxInd].localizador === null) {
+    if (
+      this.seleccionados[maxInd] !== undefined &&
+      this.seleccionados[maxInd].localizador === null
+    ) {
       this.seleccionados[maxInd].num++;
     } else {
       const hueco: ArticuloBuscador = new ArticuloBuscador();
@@ -91,21 +104,68 @@ export class ImprentaComponent {
       hueco.pvp = null;
       this.seleccionados.push(hueco);
     }
+    this.updateTable();
   }
 
   deleteLinea(ind: number): void {
     this.seleccionados.splice(ind, 1);
+    this.updateTable();
   }
 
   addLineaNum(ind: number, amount: number): void {
+    const cantidadTotal: number = this.getCantidadTotal();
+    const cantidadMax: number = this.getCantidadMax();
+
     if (amount === -1) {
       if (this.seleccionados[ind].num === 1) {
         return;
       }
       this.seleccionados[ind].num--;
+      this.updateTable();
     } else {
-      this.seleccionados[ind].num++;
+      if (this.checkCantidad(cantidadTotal + 1)) {
+        this.seleccionados[ind].num++;
+        this.updateTable();
+      }
     }
+  }
+
+  updateTable(): void {
+    this.tabla.calcularLista(
+      this.filas,
+      this.columnas,
+      this.seleccionados,
+      this.mostrarPVP
+    );
+  }
+
+  checkCantidad(num: number): boolean {
+    const cantidadMax: number = this.getCantidadMax();
+
+    if (num > this.getCantidadMax()) {
+      this.dialog
+        .alert({
+          title: "Error",
+          content:
+            "El número máximo de etiquetas que puedes imprimir es " +
+            cantidadMax,
+          ok: "Continuar",
+        })
+        .subscribe((result: boolean): void => {});
+      return false;
+    }
+    return true;
+  }
+
+  getCantidadTotal(): number {
+    return this.seleccionados.reduce(
+      (total: number, item: ArticuloBuscador): number => total + item.num,
+      0
+    );
+  }
+
+  getCantidadMax(): number {
+    return this.filas * this.columnas;
   }
 
   drop(event: CdkDragDrop<string[]>): void {
@@ -114,5 +174,6 @@ export class ImprentaComponent {
       event.previousIndex,
       event.currentIndex
     );
+    this.updateTable();
   }
 }
